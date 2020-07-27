8f530e73534a08f37fe12f27e0bb8408dfab9bb5
admin <admin@admins-MacBook-Pro.local>
2020-07-27 13:49:25 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexpashkov/nc
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"github.com/pkg/errors"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	var (
+		addr        string
+		startServer bool
+		timeout     time.Duration
+	)
+	flag.StringVar(&addr, "addr", "localhost:2222", "addr to communicate with")
+	flag.BoolVar(&startServer, "serve", false, "set to true if you want to start a server, takes precedence over write")
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "client dial timeout")
+	flag.Parse()
+	if startServer {
+		log.Printf("starting server on %s", addr)
+		if err := serve(addr); err != nil {
+			log.Fatal(errors.Wrap(err, "serving error"))
+		}
+	} else {
+		log.Printf("writing to %s", addr)
+		if err := write(addr); err != nil {
+			log.Fatal(errors.Wrap(err, "writing error"))
+		}
+	}
+}
+
+func serve(address string) error {
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return errors.Wrap(err, "failed to listen")
+	}
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			return errors.Wrap(err, "failed to accept")
+		}
+		if err := handleConn(conn); err != nil {
+			return errors.Wrap(err, "connection handling error")
+		}
+	}
+}
+
+func handleConn(conn net.Conn) error {
+	defer conn.Close()
+	_, err := io.Copy(os.Stdout, conn)
+	return err
+}
+
+func write(address string, timeout time.Duration) error {
+	conn, err := net.DialTimeout("tcp", address, timeout)
+	if err != nil {
+		return errors.Wrap(err, "dial error")
+	}
+	defer conn.Close()
+	_, err = io.Copy(conn, os.Stdin)
+	return errors.Wrap(err, "failed to copy input into the connectino")
+}
